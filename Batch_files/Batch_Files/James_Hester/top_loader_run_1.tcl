8955ead8abf7f07ca1a5a24dac6345d9e920876f
James Hester <jxh@ansto.gov.au>
2018-04-18 09:43:20 +1000

    Batch file changes and removal.

--- Batch_files/Batch_Files/James_Hester/top_loader_run_1.tcl
+++ Batch_files/Batch_Files/James_Hester/top_loader_run_1.tcl
@@ -3,16 +3,16 @@
 
 proc runtemps_up templist {
 	foreach kel_temperature $templist {
-		samplename [ concat TmCr0.9Mn0.1O3, $kel_temperature K ]
+		samplename [ concat FePO4, $kel_temperature K ]
 		# set cold head temperature
 		hset /sample/tc1/sensor/setpoint1 $kel_temperature
+		hset /sample/tc1/sensor/setpoint2 $kel_temperature
 		drive tc2_driveable $kel_temperature
 		wait 300
-		runscan stth 2.75 5.2 50 time 203
+		runscan stth 2.75 5.2 50 time 275
 	}
 }
 
-#get to base
-hset /sample/tc2/sensor/setpoint1 4
-drive tc1_driveable 7
-runtemps_up [list 5 70 200 ]
+samplename FePO4 at 4K
+runscan stth 2.75 5.2 50 time 275
+runtemps_up [list 8 11 14 16 18 20 22 24 27 ]
